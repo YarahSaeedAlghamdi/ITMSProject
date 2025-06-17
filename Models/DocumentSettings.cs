cd16d20808753f7f8cbb37f68ed312445d361e84
jood-f <joodfahad21@gmail.com>
2025-06-17 20:28:55 +0300

    update

--- Models/DocumentSettings.cs
+++ Models/DocumentSettings.cs
@@ -1,7 +1,7 @@
 public class DocumentSettings
 {
     public int DocumentTypeID;
-    public List<string> DocumentFormat  = new List<string>();
+    public List<string> DocumentFormat = new List<string>();
     public int DocumentSize;
     public string ArabicDocumentName;
     public string EnglishDocumentName;
@@ -10,26 +10,26 @@ public class DocumentSettings
 
     public void CreateNewDocument()
     {
-    
+        Console.WriteLine("A new document has been created.");
     }
 
     public void GetDocument()
     {
-        
+        Console.WriteLine("Document has been retrieved.");
     }
 
     public void GetDocument(int id)
     {
-        
+        Console.WriteLine($"Document with ID {id} has been retrieved.");
     }
 
     public void DeleteDocument()
     {
-       
+        Console.WriteLine("Document has been deleted.");
     }
 
     public void UpdateDocument()
     {
-       
+        Console.WriteLine("Document has been updated.");
     }
 }
