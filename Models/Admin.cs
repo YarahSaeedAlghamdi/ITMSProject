e2f476e0a6cf426e27f0042dd4f677c82c237bb9
jood-f <joodfahad21@gmail.com>
2025-06-17 20:20:27 +0300

    update

---
+++ Models/Admin.cs
@@ -0,0 +1,7 @@
+public class Admin : User
+{
+    public override void NavigateToHome()
+    {
+        
+    }
+}
---
+++ Models/DocumentSettings.cs
@@ -0,0 +1,35 @@
+public class DocumentSettings
+{
+    public int DocumentTypeID;
+    public List<string> DocumentFormat  = new List<string>();
+    public int DocumentSize;
+    public string ArabicDocumentName;
+    public string EnglishDocumentName;
+    public string FormURL;
+    public bool IsActiveDocument;
+
+    public void CreateNewDocument()
+    {
+    
+    }
+
+    public void GetDocument()
+    {
+        
+    }
+
+    public void GetDocument(int id)
+    {
+        
+    }
+
+    public void DeleteDocument()
+    {
+       
+    }
+
+    public void UpdateDocument()
+    {
+       
+    }
+}
