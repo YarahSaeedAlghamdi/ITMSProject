6d210ac4b45cff78b682c75a9af34bd4fd59c621
jood-f <joodfahad21@gmail.com>
2025-06-17 20:24:01 +0300

    create

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -2,6 +2,6 @@ public class Admin : User
 {
     public override void NavigateToHome()
     {
-        
+
     }
 }
---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+public class Student : User
+{
+    public string StudentPhoneNumber;
+    public double StudentGPA;
+    public int StudentNumber; // SSN
+
+    public override void NavigateToHome()
+    {
+       
+    }
+}
